7627bb6484e2d52a98c9170fd7c2eaad155393a7
deyitayo <51061806+deyitayo@users.noreply.github.com>
2020-02-05 15:51:35 -0500

    Update Next.py

--- Next.py
+++ Next.py
@@ -15,18 +15,39 @@ st.header('Procedure Information')
 
 Fluoroscopy_Time = st.number_input('Fluoroscopy Time (min)')
 
-Total_Contrast = st.number_input('Total Contrast Used (cc)')
-
 Lidocaine = st.number_input('Lidocaine 1% (cc)')
+
+Age = st.number_input('Patient age (years)')
+
 
 #Thrombolysis = int(st.number_input('Thrombolysis'))
 
 #Prolonged_Bleeding = int(st.number_input('Prolonged_Bleeding'))
 
 
-Thrombolysis = st.radio(label="Thrombolysis", options=(0, 1))
+Clotted_Access = st.radio(label="Clotted Access", options=(0, 1))
 
-Prolonged_Bleeding = st.radio(label="Prolonged Bleeding", options=(0, 1))
+Arterial = st.radio(label="Arterial", options=(0, 1))
+
+def get_pred(data_input):
+    data_input = [Fluoroscopy_Time, Lidocaine, Age, Clotted_Access, Arterial]
+    features = pd.DataFrame([data_input], columns =['Fluoroscopy_Time', 'Lidocaine', 'Age', 'Clotted_Access', 'Arterial'])
+    loaded_model = joblib.load('Rf_model.joblib')
+    pred1 = loaded_model.predict(features)
+    pred2 = pred1[0]
+    prediction = np.exp(pred2)
+    return prediction
 
 
+if st.button(label='Submit'):    
+    data_input = [Fluoroscopy_Time, Lidocaine, Age, Clotted_Access, Arterial]
+    prediction = get_pred(data_input)
+    pred = str(int(prediction)) + ' ' + 'days'
+    full_date = pd.datetime.now().date() + pd.DateOffset(days=int(prediction))
+    year = full_date.year
+    month = full_date.month
+    day = full_date.day
+    date = str(month) + '-' + str(day) + '-' + str(year)
+    st.write(f'Estimated time till next appointment: {pred}')
+    st.write(f'Estimated date of next appointment: {date}')
 
